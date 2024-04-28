adaa68b1514ba0d56e1be6a9a3539ff1b373133d
Michael Thorsager <thorsager@gmail.com>
2024-04-28 12:04:04 +0200

    Adding Content-Length if not present.
    If serving a file and the Content-Length is not set in respose
    it will now be set to the size of the file served.

--- main.go
+++ main.go
@@ -109,22 +109,42 @@ func main() {
 			w.Header().Add("Server", description)
 		}
 
-		w.WriteHeader(int(statusCodeFlag))
+		if responseBodyFlag != "" && strings.HasPrefix(responseBodyFlag, "@") {
+			filename := trimFirst(responseBodyFlag)
+			s, err := os.Stat(filename)
+			if err != nil {
+				log.Printf("error: unable to stat file: '%s'", filename)
+				return
+			}
+			if w.Header().Get("Content-Length") == "" {
+				w.Header().Add("Content-Length", strconv.Itoa(int(s.Size())))
+			}
+		}
 
 		if responseBodyFlag != "" {
 			if strings.HasPrefix(responseBodyFlag, "@") {
 				// response is filename
 				filename := trimFirst(responseBodyFlag)
+				s, err := os.Stat(filename)
+				if err != nil {
+					log.Printf("error: unable to stat file: '%s'", filename)
+					return
+				}
 				file, err := os.Open(filename)
 				if err != nil {
 					log.Printf("error: unable to open file: '%s'", filename)
 					return
 				}
 				defer quietClose(file)
+				if w.Header().Get("Content-Length") == "" {
+					w.Header().Add("Content-Length", strconv.Itoa(int(s.Size())))
+				}
+				w.WriteHeader(int(statusCodeFlag)) // start sending body
 				if _, err = io.Copy(w, file); err != nil {
 					log.Printf("error: unable to write response body: %s", err)
 				}
 			} else {
+				w.WriteHeader(int(statusCodeFlag)) // start sending body
 				if _, err := w.Write([]byte(responseBodyFlag)); err != nil {
 					log.Printf("error: unable to write response body: %s", err)
 				}
